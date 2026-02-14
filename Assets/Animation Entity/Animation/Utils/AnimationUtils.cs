509e74509d052e4e2e3b76f5bd24725e8b7687e6
XuannQuy <quynx@bravestars.com>
2026-02-14 18:15:29 +0700

    fix util

--- Assets/Animation Entity/Animation/Utils/AnimationUtils.cs
+++ Assets/Animation Entity/Animation/Utils/AnimationUtils.cs
@@ -1,28 +1,39 @@
+using Unity.Collections;
 using Unity.Entities;
 
 public static class AnimationUtils
 {
-    public static void SetAnimation(EntityManager em, Entity e, int index)
+    public static void SetAnimation(this Entity e, int index)
     {
-        if (em.HasComponent<AnimationClipIndex>(e))
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        if (World.DefaultGameObjectInjectionWorld.EntityManager.HasComponent<AnimationClipIndex>(e))
         {
             var animationClipIndex = em.GetComponentData<AnimationClipIndex>(e);
             animationClipIndex.Value = index;
-            em.SetComponentData(e, animationClipIndex);
+            ecb.SetComponent(e, animationClipIndex);
             
             // Also reset time when changing animation? The original system seems to handle this in OnChangeAnimationIndexSystem by resetting bone time.
             // But we should probably reset the entity time as well if needed?
             // The OnChangeAnimationIndexSystem resets bone time, but maybe we should let the system handle it.
         }
+        
+        ecb.Playback(em);
+        ecb.Dispose();
     }
 
-    public static void SetAnimationTime(EntityManager em, Entity e, float time)
+    public static void SetAnimationTime(this Entity e, float time)
     {
-        if (em.HasComponent<AnimationTime>(e))
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        if (em.HasComponent<AnimationRootTime>(e))
         {
-            var animationTime = em.GetComponentData<AnimationTime>(e);
+            var animationTime = em.GetComponentData<AnimationRootTime>(e);
             animationTime.Value = time;
-            em.SetComponentData(e, animationTime);
+            ecb.SetComponent(e, animationTime);
         }
+        
+        ecb.Playback(em);
+        ecb.Dispose();
     }
 }
